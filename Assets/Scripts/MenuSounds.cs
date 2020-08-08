9f23f3bb927d0959e279551ba9470067534fda2e
Charlie <chaz.com@hotmail.co.uk>
2020-08-09 00:15:45 +0100

    2nd commit.

---
+++ Assets/Scripts/MenuSounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSounds : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    public AudioClip menuClick;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void menuClickSound()
+    {
+        audioSource.clip = menuClick;
+        audioSource.Play();
+        print("menuclick play");
+    }
+
+}
